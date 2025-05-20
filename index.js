c4c7c8747c5aa47679c9314defee6a7ad4e82506
Manh1504 <evil1504@gmail.com>
2025-05-20 14:39:27 +0700

    update bai dem tu

---
+++ index.js
@@ -0,0 +1,7 @@
+let cart = [];
+let totalPrice = 0;
+
+function addToCart(productName, productPrice) {
+    cart.push({ name: productName, price: productPrice });
+    totalPrice += productPrice;
+   
